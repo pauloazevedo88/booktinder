869a0283d6fc8df951cac01f2d39c9a2252a0dec
aluno tarde <alunotarde@Altars-Mini-67.lan>
2017-08-11 18:01:43 +0100

    aula11/08

--- objectoslibrary.js
+++ objectoslibrary.js
@@ -1,11 +1,7 @@
-
-
 function Library(){  // classe Library  
-    // this.books =[ ]; //array books 
-    // this.index = 0;
     this.books = new Queue();
     this.viewdbook = new Queue();
-    this.actualbook = null;
+    this.actualbook = null; //null -> atributo vazio
    
     this.addBooks = function(book){ 
         // this.books.push(book) //estou a empurrar book para o array books
@@ -13,65 +9,35 @@ function Library(){  // classe Library
     }
     this.nextBook = function(){
         this.actualbook = this.books.dequeue();
+        if(this.actualbook != null){ 
         this.actualbook.render();
-        this.viewdbook.enqueue(this.actualbook); 
+        this.viewdbook.enqueue(this.actualbook);
+        }else{
+            $("#mainPage").hide();
+            $("#endPage").show();
+            this.counterLikes();
+        }
     }    
-    // this.nextBook = function(){  
-    //     if(this.index < this.books.length){      
-    //         var book = this.books[this.index];
-    //         book.render(); //instancia do book
-    //         this.index++;
-    //     }else{
-    //        $("#mainPage").hide();
-    //        $("#endPage").show();
-    //        //completarTabela();
-    //     }
-    // }
-
     this.like = function(){
         this.actualbook.likes++;
         this.nextBook();
-    }
+    }         
+    this.counterLikes=function(){
+        var contadorLikes=0;
+        var livroActual = this.viewdbook.dequeue();
 
-//     this.counterLikes=function(){
-//         var counterLikes = 0;
-//         var b = this.viewdbook.dequeue();
-
-//         while(b!=null){
-//             counterLikes = counterLikes + b.like;
-//             b=this.viewdbook.dequeue();
-
-//             return counterLikes;
-       
-//             }
-//         }
-// };        
-
-    // this.completarTabela = function(){
-    //      for(i=0; i<this.books.length; i++){
-    //           $("title").append(books[i].title);
-                  
-              
-            //   var table = `<tr>
-            //       <td>`
-            //              +this.title+
-            //                  `</td>  
-            //              <td>`
-            //              //+this.opinion+
-            //                  `</td>
-            //              <td>`
-            //              + showSug +`
-            //                  </td> 
-            //              </tr>`   
-                     //$("#table tbody").append(table);   
-            //         };
+        while(livroActual!=null){
+            contadorLikes = contadorLikes + livroActual.likes;
+            livroActual = this.viewdbook.dequeue();
+        }
+    $("#likes").html(contadorLikes);
+    };
 };
 
-
-function Book(image, title, descricao, links){
+function Book(image, title, description){
     this.image = image;
     this.title = title;
-    this.descricao = descricao;
+    this.description = description;
     this.likes = 0;
     this.disLikes = 0;
     //this.links = links;
@@ -79,7 +45,7 @@ function Book(image, title, descricao, links){
     this.render = function(){
         $("#image").attr("src",this.image);
         $("#title").html(this.title);
-        $("#descricao").html(this.descricao);
+        $("#descricao").html(this.description);
     }
 };
 
@@ -96,22 +62,47 @@ function Queue(){ //definicao de estrutura de dados
     }
 };  
 
-var book1 = new Book ("Aim-foi-Auschwitz.jpg","Assim foi Auschwitz", "Um livro que celebra os 70 anos do fim da Segunda Guerra Mundial","A-Amiga-Genial.jpg");
+function init(paramPesquisa){
+    $.get("https://www.googleapis.com/books/v1/volumes?q="+encodeURI(paramPesquisa)) //get ajax; encodeURI é para contemplar espaços ou acentos
+    .done(function(data){
+        for(i=0; i<data.items.length; i++){
+            var livrosGoogle = data.items[i];
+            
+            var title = livrosGoogle.volumeInfo.title;           
+            var image = livrosGoogle.volumeInfo.imageLinks.thumbnail;
+            var description = livrosGoogle.volumeInfo.description;
+            //console.log(livrosGoogle);
+            livrosGoogle = new Book (image, title, description);
+            library.addBooks(livrosGoogle);           
+        }
+      library.nextBook();  
+    })
+    .fail(function(data){
+        // for(i=0; i<data.items.length; i++){
+        //     var livrosGoogle = data.items[i];
+        //     var title = livrosGoogle["volumeInfo"]["title"];
+        //     var description = livrosGoogle["volumeInfo"]["description"];
+        //     var image = livrosGoogle["volumeInfo"]["image"]
+            console.log(livrosGoogle);
+        }
+    )
+};
 
-var book2 = new Book ("PRINCIPE-NABO-O.jpg","O Príncipe Nabo","Ilse Losa (Autor) Manuela Bacelar (Ilustração) em Português","O-Mundo-Em-Que-Vivi.jpg");
+init("ola");
 
-var book3 = new Book ("1507-1.jpg","If Not Now, When?","Primo Levi (Autor) Lançado em junho de 2017 Edição em Inglês","1507-1-1.jpg");
 
-var book4 = new Book ("Historia-do-Novo-Nome.jpg","História do Novo Nome","A Amiga Genial Vol 2 Elena Ferrante (Autor) Lançado em julho de 2015 Edição em Português","1507-1-2.jpg");
+// var book2 = new Book ("PRINCIPE-NABO-O.jpg","O Príncipe Nabo","Ilse Losa (Autor) Manuela Bacelar (Ilustração) em Português","O-Mundo-Em-Que-Vivi.jpg");
+
+// var book3 = new Book ("1507-1.jpg","If Not Now, When?","Primo Levi (Autor) Lançado em junho de 2017 Edição em Inglês","1507-1-1.jpg");
+
+// var book4 = new Book ("Historia-do-Novo-Nome.jpg","História do Novo Nome","A Amiga Genial Vol 2 Elena Ferrante (Autor) Lançado em julho de 2015 Edição em Português","1507-1-2.jpg");
 
 var library = new Library();
 
-library.addBooks(book1);
-library.addBooks(book2);
-library.addBooks(book3);
-library.addBooks(book4);
-
-library.nextBook();
+// library.addBooks(book1);
+// library.addBooks(book2);
+// library.addBooks(book3);
+// library.addBooks(book4);
 
 $("#buttonStart").click(function(){
     $("#startPage").hide();
@@ -122,88 +113,3 @@ $("#buttonStart").click(function(){
 $("#buttonLike").click(function(){  
     library.like();
 }); 
-
-// var librarys = [book1, book2, book3, book4];
-
-// function preencher(){
-
-//     var insertHTML = `
-//         <div class = "book col-md-4-offset-2 col-sm-8-offset-2 col-sm-offset-2">
-//         <img>
-//         <h1></h1>
-//         <p></p>
-//         <a></a>
-//         </div>           
-//         `
-
-//     $.each(library,function(index,book){
-//         $("#bookContainer").append(insertHTML);
-//         $ultimoInserido=$(".book:last-child");
-//         $("img", $ultimoInserido).attr("src", book.image);
-//         $("h1", $ultimoInserido).text(book.title);
-//         $("p", $ultimoInserido).text(book.descricao);
-//         $("a", $ultimoInserido).attr("href", book.links);
-//     });        
-// };
-// preencher();
-
-// $(".book:first-child").addClass("active")
-    
-//    $("#buttonLike").click(function(){
-
-//    //     $currentBook = $(".book.active");
-//    //     $nextBook = $currentBook.next(".book");
-
-//    //     var index = $(".book").index($currentBook);
-//    //     library[index].opinion = $(this).attr('data-Opinion')    
-    
-//          if(library.index < library.books.length){
-//              library.books[library.index].likes++;
-//              library.nextBook();
-//    // //         $currentBook.removeClass("active");
-//    // //         $nextBook.addClass("active");  
-//        }else{
-//            library.books[library.index].likes++;
-//            $("#mainPage").hide();
-//            $("#endPage").show();
-//            completarTabela();
-//            // count();
-//          }
-//      });
-
-// function completarTabela(){
-//     $.each(library,function(index, book){   
-//         var showSug = 0;   
-//         if(book.opinion=="dislike"){
-//             showSug = `<img src="`+book.sugestoes+`">`
-//             }else{
-//             showSug = "-";   
-//             }   
-//         var table = `<tr>
-//                         <td>`
-//                             +this.title+
-//                         `</td>  
-//                         <td>`
-//                             +this.opinion+
-//                         `</td>
-//                         <td>`
-//                             + showSug +`
-//                         </td> 
-//                     </tr>`   
-//         $("#table tbody").append(table);   
-// })};
-
-    // function count(){   
-    //     var likes = 0;
-    //     var dislikes = 0;   
-    //         $.each(library, function(index, book){   
-    //         if(book.opinion=="like"){
-    //             likes++;
-    //         }
-    //         else{
-    //             dislikes++;
-    //         }
-     
-    //         $("#um").text(likes);
-    //         $("#dois").text(dislikes);
-    // }};
